f420c9ea786cc7ab4daafaa351faa81b1bc9791c
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-08-16 14:55:27 +1000

    my.packages: init

--- my/config.nix
+++ my/config.nix
@@ -3,6 +3,7 @@
   unison.enableX11 = false;
   packageOverrides = pkgs: {
     my = {
+      packages = import ./packages.nix pkgs.pkgs;
       xmonad-exe = pkgs.haskellPackages.callPackage ./xmonad-exe.nix {};
       x-terminal-emulator = pkgs.callPackage ./x-terminal-emulator.nix {};
     };
---
+++ my/packages.nix
@@ -0,0 +1,55 @@
+pkgs:
+
+with pkgs;
+
+[
+  acpi
+  anki
+  binutils
+  cabal2nix
+  chromium
+  evince
+  feh
+  ffmpeg_3_2
+  file
+  firefox-esr-60
+  gimp
+  git
+  gitAndTools.git-annex
+  gitAndTools.gitRemoteGcrypt
+  gmrun
+  gnome3.dconf
+  gnome3.defaultIconTheme
+  gnome3.nautilus
+  gnupg
+  hicolor-icon-theme
+  mailutils
+  mkvtoolnix
+  mpv
+  networkmanagerapplet
+  pavucontrol
+  powerline-fonts
+  redshift
+  steam
+  steam-run
+  sudo
+  taskwarrior
+  transmission-gtk
+  trayer
+  unison
+  unzip
+  vimHugeX
+  weechat
+  wget
+  xbindkeys
+  xfce.terminal
+  xscreensaver
+  youtube-dl
+
+  haskellPackages.myanimelist-export
+  haskellPackages.passman-cli
+  haskellPackages.xmobar
+
+  my.xmonad-exe
+  my.x-terminal-emulator
+]

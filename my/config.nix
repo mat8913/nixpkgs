a6126f04b328c80478bf541fe60f794ecb85b3f9
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-10-04 16:17:17 +1000

    my.packages: init

--- my/config.nix
+++ my/config.nix
@@ -1,3 +1,8 @@
 {
   unison.enableX11 = false;
+  packageOverrides = pkgs: {
+    my = {
+      packages = import ./packages.nix pkgs.pkgs;
+    };
+  };
 }
---
+++ my/packages.nix
@@ -0,0 +1,25 @@
+pkgs:
+
+with pkgs;
+
+[
+  acpi
+  chromium
+  ffmpeg_3_2
+  firefox-esr-60
+  git
+  gitAndTools.git-annex
+  gitAndTools.gitRemoteGcrypt
+  gnupg
+  mkvtoolnix
+  mpv
+  powerline-fonts
+  taskwarrior
+  unison
+  vimHugeX
+  weechat
+  youtube-dl
+
+  haskellPackages.myanimelist-export
+  haskellPackages.passman-cli
+]

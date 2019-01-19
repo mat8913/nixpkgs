2fffd40485d223bde3ff0230434ee51f305fa69e
Matthew Harm Bekkema <id@mbekkema.name>
2019-01-19 16:01:57 +1100

    ffmpeg_3_2: 3.2.12 -> 3.2.13

--- pkgs/development/libraries/ffmpeg/3.2.nix
+++ pkgs/development/libraries/ffmpeg/3.2.nix
@@ -6,7 +6,7 @@
 
 callPackage ./generic.nix (args // rec {
   version = "${branch}";
-  branch = "3.2.12";
-  sha256 = "1n2b9zg3zyjysshan3hdwk8wbn1pyc86yj3w1f9b107cswhwzqs5";
+  branch = "3.2.13";
+  sha256 = "0h10zvi6q9lqdbk3gb4zhlxggcgqa04s0w2gwaxcz0cnchfbddg5";
   darwinFrameworks = [ Cocoa CoreMedia ];
 })
